3fdfa226193169f50d518c62e4c2c0b639d10115
Brian Zoetewey <brian.zoetewey@cru.org>
2022-10-14 14:18:10 -0400

    Update application for ECS multi-container. Fix redis implementation and ENV used. Remove build/dist files from git.

--- api/api/redis.ts
+++ api/api/redis.ts
@@ -1,11 +1,12 @@
 import redis from 'redis';
 
 const redisConfig = {
-  host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT || '6379',
+  host: process.env.SESSION_REDIS_HOST || 'localhost',
+  port: process.env.SESSION_REDIS_PORT || '6379',
+  dbIndex: process.env.SESSION_REDIS_DB_INDEX || '0'
 }
 
-export const clientRedis = redis.createClient({ host: redisConfig.host, port: Number(redisConfig.port) });
+export const clientRedis = redis.createClient({ host: redisConfig.host, port: Number(redisConfig.port), db: redisConfig.dbIndex });
 clientRedis.once('ready', () => console.log('clientRedis connected'));
 clientRedis.on('error', (err) => console.log(err));
 
