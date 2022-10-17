39f446aa4bbd055d4f12cd0ad1801f2adbd6b223
Brian Zoetewey <brian.zoetewey@cru.org>
2022-10-17 11:59:59 -0400

    Reset redis.ts back to original for testing

--- api/api/redis.ts
+++ api/api/redis.ts
@@ -1,17 +1,14 @@
 import redis from 'redis';
 
 const redisConfig = {
-  host: 'localhost',
-  // host: process.env.SESSION_REDIS_HOST || 'localhost',
-  port: process.env.SESSION_REDIS_PORT || '6379',
-  dbIndex: '0',
-  // dbIndex: process.env.SESSION_REDIS_DB_INDEX || '0'
+  host: process.env.REDIS_HOST || 'localhost',
+  port: process.env.REDIS_PORT || '6379',
 }
 
-export const clientRedis = redis.createClient({ host: redisConfig.host, port: Number(redisConfig.port), db: redisConfig.dbIndex });
+export const clientRedis = redis.createClient({ host: redisConfig.host, port: Number(redisConfig.port) });
 clientRedis.once('ready', () => console.log('clientRedis connected'));
 clientRedis.on('error', (err) => console.log(err));
 
 export function setRedis(key, data) {
-  clientRedis.set(key, JSON.stringify(data)); 
+  clientRedis.set(key, JSON.stringify(data));
 }
